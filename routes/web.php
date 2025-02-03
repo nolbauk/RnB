a9baa5087d1043207813b68b8f8406765139f7a4
roymahendra37 <roymahendra37@gmail.com>
2025-02-03 14:05:08 +0700

    halaman dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\DashboardController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
