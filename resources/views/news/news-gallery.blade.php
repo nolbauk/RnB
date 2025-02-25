a45d08c08fad6d37bd4dea9336723b8f394a87de
nolbauk <bedegelen062218@gmail.com>
2025-02-25 15:28:17 +0700

    News 0.1

--- resources/views/news/news-gallery.blade.php
+++ resources/views/news/news-gallery.blade.php
@@ -60,7 +60,7 @@
     <div class="container mt-4" style="padding-top: 60px;">
         <!-- Top box -->
         <div class="row">
-            <div class="col-12 text-white p-4 text-center">
+            <div class="col-12 text-white p-4 text-center outline-box">
                 <div class="row">
                     <div class="col-sm-6">
                         <h1 class="banner_taital">Roiy</h1>
@@ -76,8 +76,8 @@
 
         <!-- Two boxes below -->
         <div class="row mt-3">
-            <div class="col-md-6 text-white p-4 text-center">
-                <div class="row">
+            <div class="col-md-6 text-white text-center">
+                <div class="row outline-box">
                     <div class="col-sm-6">
                         <h4 class="banner_taital_second">Roiy</h4>
                         <p class="banner_text_second">Roiy adalah seorang budak cinta dari seorang manusia bernama Celin</p>
@@ -88,8 +88,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 text-white p-4 text-center">
-                <div class="row">
+            <div class="col-md-6 text-white text-center">
+                <div class="row outline-box">
                     <div class="col-sm-6">
                         <h4 class="banner_taital_second">Roiy</h4>
                         <p class="banner_text_second">Roiy adalah seorang budak cinta dari seorang manusia bernama Celin</p>
