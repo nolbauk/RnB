3748f3705e2f7c9ec9e7b761b493a09b93ef2677
nolbauk <bedegelen062218@gmail.com>
2025-02-28 16:00:52 +0700

    Lapo neh iki

--- resources/views/forum/discuss-detail.blade.php
+++ resources/views/forum/discuss-detail.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Art</title>
+    <title>DISCUSSION DETAIL</title>
     <link rel="stylesheet" href="{{ asset('/css/forum.css') }}">
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
